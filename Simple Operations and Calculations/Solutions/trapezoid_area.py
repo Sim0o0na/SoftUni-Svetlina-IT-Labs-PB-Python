6f3d16e388e6ad47cb671790c120839771f85a09
Simona Simeonova <ssimeonova159@gmail.com>
2018-10-22 13:36:02 +0300

    Added Simple Operations and Calculations Solutions

---
+++ Simple Operations and Calculations/Solutions/trapezoid_area.py
@@ -0,0 +1,12 @@
+# Read all input
+# Convert input to numbers
+b1 = float(input())
+b2 = float(input())
+h = float(input())
+
+# Calculate trapezoid area by (b1 + b2) * h / 2
+# Save area in a variable
+trapezoid_area = (b1 + b2) * h / 2
+
+# Print area
+print(trapezoid_area)
---
+++ Simple Operations and Calculations/Solutions/triangle_area.py
@@ -0,0 +1,5 @@
+# Read input
+# Calculate area pi * r * r and save in variable
+# Calculate perimeter pi * 2 * r and save in variable
+# print area
+# print perimeter
